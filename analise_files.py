2a13d027c6d3b32e612a1891549dd865f2dd269c
Kirill2434 <cool.yn20@yandex.ru>
2023-03-17 17:35:56 +0300

    Актуальное ревью

--- analise_files.py
+++ analise_files.py
@@ -1,5 +1,4 @@
 import openpyxl as op
-from openpyxl.styles.numbers import BUILTIN_FORMATS
 
 path = r'C:\generation_results\new.xlsx'
 resurse_path = r'C:\generation_results\fin.xlsx'
@@ -21,7 +20,7 @@ def analise_column(file, column_inx, min_row):
         for row in sheet.iter_cols(min_col=col, max_col=col, min_row=min_row):
             for cell in row:
                 # Отбираем все значения кроме None
-                if type(cell.value) is str:
+                if isinstance(cell.value, str):
                     try:
                         # преобразуем в float, если не получилось, значит имеем дело не с числом
                         float(cell.value)
@@ -33,7 +32,7 @@ def analise_column(file, column_inx, min_row):
                         sheet[coordinate_of_cells] = None
                         # очищаем некорректную ячейку и полностью удаляем строку
                         sheet.delete_rows(number_of_rows)
-                    if type(cell.value) is str and '.' in cell.value:
+                    if isinstance(cell.value, str) and '.' in cell.value:
                         # округляем очищенные данные
                         coordinate_of_float_cells = cell.coordinate
                         float_format = float(cell.value)
@@ -49,25 +48,4 @@ def analise_column(file, column_inx, min_row):
     return 'Проверка закончена'
 
 
-# print(analise_column(resurse_path, [13, 14, 15], 2))
-
-
-column_inx = [6, 23]
-workbook = op.load_workbook(r'C:\generation_results\fin.xlsx')
-sheet = workbook.active
-for col in column_inx:
-    for row in sheet.iter_cols(min_col=col, max_col=col, min_row=2):
-        for cell in row:
-            try:
-                # преобразуем в datetime, если не получилось, значит имеем дело не с числом
-                coordinate_of_cells = cell.coordinate
-                sheet[coordinate_of_cells].number_format = BUILTIN_FORMATS[14]
-            except ValueError:
-                # если не получислось преобразовать в datetime, то
-                # находим кординаты ячейки и номер строки и
-                # очищаем некорректную ячейку
-                coordinate_of_cells = cell.coordinate
-                sheet[coordinate_of_cells] = None
-
-        workbook.save(path)
-
+print(analise_column(resurse_path, [12, 13, 14, 15], 2))
